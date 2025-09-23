34f87eb43279254a5580a2609247f6f6abd1f887
Salih <salihmorina555@gmail.com>
2025-09-23 11:36:52 +0200

    fast_api_ml

---
+++ code-alongs/fast_api_ml/data_processing.py
@@ -0,0 +1,27 @@
+import pandas as pd
+from constants import DATA_PATH
+from pydantic import BaseModel, Field
+df = pd.read_csv(DATA_PATH / "iris.csv", index_col=0)
+
+class IrisData:
+    def __init__(self):
+        self.df = df
+
+    def to_json(self):
+        return self.df.to_dict(orient="records")
+
+# request/response schemas
+
+class IrisInput(BaseModel):
+    SepalLengthCm: float = Field()
+    SepalWidthCm: float = Field()
+    PetalLengthCm: float = Field()
+    PetalWidthCm: float = Field()
+
+class PredictionOutput(BaseModel):
+    predicted_flower: str
+
+
+if __name__ == "__main__":
+    irirs = IrisData()
+    print(irirs.to_json())
